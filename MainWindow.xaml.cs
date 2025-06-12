b093f0cf2d781110c0ba2c90f24416635c9aaf35
shumeikoalex-byte <shumeiko.alexey@gmail.com>
2025-06-12 16:39:13 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using EConstruction.ViewModels;
+
+namespace EConstruction
+{
+    /// <summary>
+    /// Логіка взаємодії для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DataContext встановлюється в XAML
+        }
+    }
+}
