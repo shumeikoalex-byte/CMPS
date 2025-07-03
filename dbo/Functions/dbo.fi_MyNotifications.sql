fcf37077eefec1bc63f8223eed7e98080bc7bbe7
shumeikoalex-byte <shumeiko.alexey@gmail.com>
2025-07-03 17:26:37 +0300

    Add files via upload

---
+++ dbo/Functions/dbo.fi_MyNotifications.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [dbo].[fi_MyNotifications]()
+RETURNS TABLE
+AS
+RETURN
+	SELECT *
+	FROM [dbo].Notifications
+	WHERE RecipientId = (
+		SELECT [AgentId]
+		FROM dbo.LocalUsers 
+		WHERE [Id] = CONVERT(INT, SESSION_CONTEXT(N'UserId'))
+	);
